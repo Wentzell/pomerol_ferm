9f39ef9abd56cfa4de4d13f00bfa58e143b5fd6b
Nils Wentzell <nils.wentzell@gmail.com>
2016-07-07 20:34:42 +0200

    fix of scripts

--- scripts/merge.sh
+++ scripts/merge.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 #if [ ! -f 1111.dat ]; then
-	#cat chi_ferm_1111_w1* > temp_dat
+	#cat chi_1111_w1* > temp_dat
 	#LC_ALL=C sort -g -k1 -k2 -k3 temp_dat > 1111.dat
 #fi
 
 if [ ! -f 1010.dat ]; then
-	cat chi_ferm_1010_w1* > temp_dat
+	cat chi_1010_w1* > temp_dat
 	LC_ALL=C sort -g -k1 -k2 -k3 temp_dat > 1010.dat
 fi
 
 #if [ ! -f 1001.dat ]; then
-	#cat chi_ferm_1001_w1* > temp_dat
+	#cat chi_1001_w1* > temp_dat
 	#LC_ALL=C sort -g -k1 -k2 -k3 temp_dat > 1001.dat
 #fi
 
